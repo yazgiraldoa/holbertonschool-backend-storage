dd7eedc0836c87c66b6dc704e8dab28c72893dea
yazgiraldoa <edygiraldo.97@gmail.com>
2022-05-03 17:51:59 -0500

    add function that updates a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function that changes all topics of a
+school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Function that changes all topics of a
+    school document based on the name
+    """
+    mongo_collection.update_many({ "name": name }, { '$set': { "topics": topics } })
