290ce5ae81c8590fc475e61266e9f8e6a4befa29
yazgiraldoa <edygiraldo.97@gmail.com>
2022-05-03 00:02:33 -0500

    add project

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Function that lists all documents in a collection
+    """
+    return mongo_collection.find()
